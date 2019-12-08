5e472217b9c18f54c846d228fa7a9c9a79f4dc99
DongGyuLee (My PC) <ledogy333@gmail.com>
2019-12-08 14:14:21 +0900

    sound bug check
    체크용 깃

--- StartState.py
+++ StartState.py
@@ -4,33 +4,35 @@ import GameWorldManager
 import MainState
 
 name = "StartState"
+
 image = None
 ready_font = None
-bgm = None
+ready_bgm = None
 
 
 def enter_state():
     global image
     global ready_font
-    global bgm
+    global ready_bgm
 
     image = pico2d.load_image('resource\\start_image.jpg')
     ready_font = pico2d.load_font('resource\\NecroSans.ttf', 20)
-    bgm = pico2d.load_music('resource\\sound\\start_bgm.mp3')
 
-    bgm.set_volume(100)
-    bgm.repeat_play()
+    ready_bgm = pico2d.load_music('resource\\sound\\start_bgm.mp3')
+    ready_bgm.set_volume(90)
+    ready_bgm.play(3)
+
     pass
 
 
 def exit_state():
     global image
     global ready_font
-    global bgm
+    global ready_bgm
 
     del image
     del ready_font
-    del bgm
+    del ready_bgm
     pass
 
 
@@ -64,12 +66,12 @@ def update():
 
 def draw():
     global image
+    global ready_font
+
     pico2d.clear_canvas()
 
     image.draw(pico2d.get_canvas_width()//2, pico2d.get_canvas_height()//2)
     ready_font.draw(pico2d.get_canvas_width()//2, pico2d.get_canvas_height()//2, str("Press Enter"))
-
-    print(bgm.get_volume())
 
     pico2d.update_canvas()
     pass
